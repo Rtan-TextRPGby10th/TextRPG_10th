6653aa0c634015b30dac09e58d96a6edbfd4f122
HYOSIN219 <38967913+HYOSIN219@users.noreply.github.com>
2025-02-07 18:52:15 +0900

    [add] 기타 도감에 다양한 아이템 추가

--- TextRPG_by_10th/MiscItem.cs
+++ TextRPG_by_10th/MiscItem.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Numerics;
 
-public class MiscItem
+public class MiscItem       //기타 아이템 클래스
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public int Amount { get; set; } // 수량
+    public int Amount { get; set; }
     public int Price { get; set; }
 
     public MiscItem()
@@ -30,11 +30,13 @@ public class MiscItem
         Price = price;
     }
 
-    public static List<MiscItem> GetMiscCatalog()
+    public static List<MiscItem> GetMiscCatalog()                       //기타 도감
     {
-        return new List<MiscItem>
+        return new List<MiscItem>                                       //기타 아이템 데이터 추가하려면 여기에 수정
         {
-            new MiscItem(1, "구울의 머리", "구울을 처치하면 간혹 떨어지는 기괴한 머리", 1, 3000)
+            new MiscItem(10001, "구울의 머리", "구울을 처치하면 간혹 떨어지는 기괴한 머리", 1, 500),
+            new MiscItem(10002, "슬라임의 점액", "미끌미끌. 만지면 기분이 좋다", 1, 200),
+            new MiscItem(10003, "거대 슬라임의 점액", "매우 희귀하다. 비싼값에 거래된다", 1, 2000)
         };
     }
 }
