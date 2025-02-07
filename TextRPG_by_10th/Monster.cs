0e6829a94ebcef7277ccbfe052858166d013f472
ManJin12 <hh5570hh@naver.com>
2025-02-07 11:09:39 +0900

    Creature, Player, Monster 클래스 추가
    Creature, Player, Monster 클래스 추가

---
+++ TextRPG_by_10th/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_by_10th
+{
+    internal class Monster : Creature
+    {
+        int ClearGold = 0;
+        
+        public Monster(string name,float health, float attackPower,float defense, int lv, int clearGold)
+                        : base(name, health, attackPower, defense, lv)
+        {
+            ClearGold = clearGold;
+        }
+    }
+
+    class MonsterType1 : Monster
+    {
+        public MonsterType1(string name, float health, float attackPower, float defense, int lv, int clearGold)
+                        : base(name, health, attackPower, defense,lv, clearGold)
+        {
+
+        }
+    }
+}
