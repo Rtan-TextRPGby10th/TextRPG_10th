f921802058c4b9342e77f56fbdc7732ef7d4b8e6
jj930220 <jj930220@gmail.com>
2025-02-11 17:41:02 +0900

    [ADD] 아이템 json으로 저장 및 로드

--- TextRPG_by_10th/ConsumableItem.cs
+++ TextRPG_by_10th/ConsumableItem.cs
@@ -36,18 +36,24 @@ public class ConsumableItem     //소모품 아이템 클래스
         Price = price;
     }
 
-    // Lazy<T>를 사용해 소모품 도감을 한 번만 로드
-    private static readonly Lazy<List<ConsumableItem>> _consumableCatalog = new Lazy<List<ConsumableItem>>(() => new List<ConsumableItem>
+    private static List<ConsumableItem> _consumableCatalog;
+
+    public static void SetConsumeItem(List<ConsumableItem> list)
     {
-        //                    도감id               턴, 값                            수량,가격
-            new ConsumableItem(1001, "힐링포션(소)", 0, 30, "30의 체력을 회복하는 포션.", 1, 100),
-            new ConsumableItem(1002, "힐링포션(중)", 0, 50, "50의 체력을 회복하는 포션.", 1, 170),
-            new ConsumableItem(1003, "힐링포션(대)", 0, 70, "70의 체력을 회복하는 포션.", 1, 220),
-            new ConsumableItem(1004, "맹독포션", 5, -1, "무기에 바를 수 있는 맹독성 포션.", 1, 120)
-    });
+        _consumableCatalog = list.ToList();
+    }
+    // Lazy<T>를 사용해 소모품 도감을 한 번만 로드
+    //private static readonly Lazy<List<ConsumableItem>> _consumableCatalog = new Lazy<List<ConsumableItem>>(() => new List<ConsumableItem>
+    //{
+    //    //                    도감id               턴, 값                            수량,가격
+    //        new ConsumableItem(1001, "힐링포션(소)", 0, 30, "30의 체력을 회복하는 포션.", 1, 100),
+    //        new ConsumableItem(1002, "힐링포션(중)", 0, 50, "50의 체력을 회복하는 포션.", 1, 170),
+    //        new ConsumableItem(1003, "힐링포션(대)", 0, 70, "70의 체력을 회복하는 포션.", 1, 220),
+    //        new ConsumableItem(1004, "맹독포션", 5, -1, "무기에 바를 수 있는 맹독성 포션.", 1, 120)
+    //});
 
     public static List<ConsumableItem> GetItemCatalog()
     {
-        return _consumableCatalog.Value;
+        return _consumableCatalog;
     }
 }
--- TextRPG_by_10th/DataLoad.cs
+++ TextRPG_by_10th/DataLoad.cs
@@ -11,15 +11,40 @@ namespace TextRPG_by_10th
     internal class DataLoad
     {
         private static string miscItemPath = "../../../../TextRPG_by_10th/MiscItemData.json";
+        private static string equipItemPath = "../../../../TextRPG_by_10th/EquipItemData.json";
+        private static string consumeItemPath = "../../../../TextRPG_by_10th/ConsumeItemData.json";
+
 
         public static void SaveMiscItemData()
         {
             // 소재 데이터 생성(1회용)
-            var ab= MiscItem.GetMiscCatalog();
+            var data= MiscItem.GetMiscCatalog();
 
             string path = miscItemPath;
 
-            string json = JsonConvert.SerializeObject(ab);
+            string json = JsonConvert.SerializeObject(data);
+
+            File.WriteAllText(path, json);
+        }
+        public static void SaveEquipItemData()
+        {
+            // 장비 데이터 생성(1회용)
+            var data = Equipment.GetEquipmentCatalog();
+
+            string path = equipItemPath;
+
+            string json = JsonConvert.SerializeObject(data);
+
+            File.WriteAllText(path, json);
+        }
+        public static void SaveConsumeItemData()
+        {
+            // 소재 데이터 생성(1회용)
+            var data = ConsumableItem.GetItemCatalog();
+
+            string path = consumeItemPath;
+
+            string json = JsonConvert.SerializeObject(data);
 
             File.WriteAllText(path, json);
         }
@@ -27,20 +52,45 @@ namespace TextRPG_by_10th
         public static void LoadMiscItemData()
         {
             // 소재아이템 데이터 로드
-
             string path = miscItemPath;
+
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+
+                MiscItem.SetMiscItem(JsonConvert.DeserializeObject<List<MiscItem>>(json));
+
+                MiscItem.GetMiscCatalog();
+            }
+        }
+
+        public static void LoadConsumeItemData()
+        {
+            // 소재아이템 데이터 로드
+            string path = consumeItemPath;
 
             if (File.Exists(path))
             {
                 string json= File.ReadAllText(path);
 
-                MiscItem.SetMiscItem(JsonConvert.DeserializeObject<List<MiscItem>>(json));
+                ConsumableItem.SetConsumeItem(JsonConvert.DeserializeObject<List<ConsumableItem>>(json));
 
-
-                MiscItem.GetMiscCatalog();
+                ConsumableItem.GetItemCatalog();
             }
+        }
+        public static void LoadEquipItemData()
+        {
+            // 소재아이템 데이터 로드
+            string path = equipItemPath;
 
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
 
+                Equipment.SetEquipItem(JsonConvert.DeserializeObject<List<Equipment>>(json));
+
+                Equipment.GetEquipmentCatalog();
+            }
         }
     }
 }
